0da401a928ed92597f37dc2fd8cbf8594449385f
olesia <ldsolesya@gmail.com>
2025-07-05 10:50:28 +0100

    hw 3

---
+++ src/components/App/App.tsx
@@ -0,0 +1,48 @@
+import { useState } from 'react';
+import toast, { Toaster } from 'react-hot-toast';
+import fetchMovies from '../../services/movieService';
+import type Movie from '../../types/movie';
+import SearchBar from '../SearchBar/SearchBar';
+import MovieGrid from '../MovieGrid/MovieGrid';
+import Loader from '../Loader/Loader';
+import ErrorMessage from '../ErrorMessage/ErrorMessage';
+import MovieModal from '../MovieModal/MovieModal';
+
+const notify = () => toast('No movies found for your request.');
+
+export default function App() {
+    const [movies, setMovies] = useState<Movie[]>([]);
+    const [isLoading, setIsLoading] = useState(false);
+    const [isError, setIsError] = useState(false);
+    const [selectedMovie, setSelectedMovie] = useState<Movie | null>(null);
+
+    const handleSearch = async (query: string) => {        
+        try {
+            setIsError(false);
+            setMovies([]);
+            setIsLoading(true);
+            const data = await fetchMovies(query);
+            if (data.results.length === 0) {
+                notify()
+                return
+            }
+            setMovies(data.results)
+        } catch {
+            setIsError(true);
+        } finally {
+            setIsLoading(false)
+        }
+    };
+
+    return (
+        <>
+            <SearchBar onSubmit={handleSearch} />
+            <Toaster />            
+            {isLoading ? <Loader /> : <MovieGrid movies={movies} onSelect={setSelectedMovie } />}
+            {isError && <ErrorMessage />}
+            {selectedMovie && (
+            <MovieModal movie={selectedMovie} onClose={() => setSelectedMovie(null)} />
+            )}
+        </>
+    ) 
+}
---
+++ src/services/movieService.ts
@@ -0,0 +1,28 @@
+import type Movie from '../types/movie'
+const myToken = import.meta.env.VITE_TMDB_TOKEN;
+import axios from 'axios';
+
+interface FetchMoviesResponse {
+    page: number;
+    results: Movie[];
+    total_pages: number;
+    total_results: number;
+  }
+
+export default async function fetchMovies(query: string): Promise<FetchMoviesResponse> {
+    const baseURL = 'https://api.themoviedb.org/3';
+    const endPoint = '/search/movie';
+    const url = baseURL + endPoint;
+         
+    const params = {
+        query,
+    };
+         
+    const headers = {
+        Authorization: myToken,
+    };
+         
+    const res = await axios.get(url, { headers, params });
+    return res.data;
+};
+    
